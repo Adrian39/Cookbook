7e8662b14d507d3147db53f6ce7a33e891a3af92
Adrian39 <adrian.live.39@gmail.com>
2016-06-18 14:20:59 -0600

    SQLite database handler: created tables

--- app/src/main/java/com/android/lopez/cookbook/DBHandler.java
+++ app/src/main/java/com/android/lopez/cookbook/DBHandler.java
@@ -1,8 +1,10 @@
 package com.android.lopez.cookbook;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 /**
  * Created by JacoboAdrian on 5/4/2016.
@@ -22,39 +24,63 @@ public class DBHandler extends SQLiteOpenHelper {
     private static final String TABLE_3_NAME = "RecipeIngredient", T3_FK_RECIPE = "RecipeID",
             T3_FK_INGREDIENT = "IngredientID", T3_QUANTITY = "Quantity";                                //Table 3 = RecipeIngredientLink Database
 
+    private Context context;
+
 
     public DBHandler(Context context) {
         super(context, DB_NAME, null, SCHEME_VERSION);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         //----TABLE 1 QUERY: RECIPES TABLE----//
-        db.execSQL("CREATE TABLE " + TABLE_1_NAME + "(" + T1_KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                T1_NAME + " VARCHAR(50), " +
-                T1_CATEGORY + " VARCHAR(50), " +
-                T1_TIME + " VARCHAR(50), " +
-                T1_PREPARATION + " TEXT, " +
-                T1_SERVINGS + " INTEGER, " +
-                T1_IMAGE_FILE + " BLOB);");
+
+        try {
+            db.execSQL("CREATE TABLE " + TABLE_1_NAME + "(" + T1_KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    T1_NAME + " VARCHAR(50), " +
+                    T1_CATEGORY + " VARCHAR(50), " +
+                    T1_TIME + " VARCHAR(50), " +
+                    T1_PREPARATION + " TEXT, " +
+                    T1_SERVINGS + " INTEGER, " +
+                    T1_IMAGE_FILE + " BLOB);");
+        }
+        catch (SQLException e){
+            Toast.makeText(context, "An error has occurred when attempting to create the 'Recipes Table'", Toast.LENGTH_LONG).show();
+        }
 
         //----TABLE 2 QUERY: INGREDIENTS TABLE----//
-        db.execSQL("CREATE TABLE " + TABLE_2_NAME + "(" + T2_KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                T2_NAME + " VARCHAR(50), " +
-                T2_CATEGORY + " VARCHAR(50), " +
-                T2_IN_STOCK + " INTEGER, " +
-                T2_NEEDED + " INTEGER);");
+        try {
+            db.execSQL("CREATE TABLE " + TABLE_2_NAME + "(" + T2_KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    T2_NAME + " VARCHAR(50), " +
+                    T2_CATEGORY + " VARCHAR(50), " +
+                    T2_IN_STOCK + " INTEGER, " +
+                    T2_NEEDED + " INTEGER);");
+        }
+        catch (SQLException e){
+            Toast.makeText(context, "An error has occurred when attempting to create the 'Ingredient Table'", Toast.LENGTH_LONG).show();
+        }
 
         //----TABLE 3 QUERY: RECIPE/INGREDIENT LINK TABLE----//
-        db.execSQL("CREATE TABLE " + TABLE_3_NAME + "(" + T3_FK_INGREDIENT + " INTEGER, " +
-                T3_FK_RECIPE + " INTEGER, " +
-                T3_QUANTITY + " INTEGER);");
+        try {
+            db.execSQL("CREATE TABLE " + TABLE_3_NAME + "(" + T3_FK_INGREDIENT + " INTEGER, " +
+                    T3_FK_RECIPE + " INTEGER, " +
+                    T3_QUANTITY + " INTEGER);");
+        }
+        catch (SQLException e){
+            Toast.makeText(context, "An error has occurred when attempting to create the 'Link Table'", Toast.LENGTH_LONG).show();
+        }
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_1_NAME);
-        onCreate(db);
+        try{
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_1_NAME);
+            onCreate(db);
+        }
+        catch (SQLException e){
+            Toast.makeText(context, "An error has occurred when attempting to delete the Database", Toast.LENGTH_LONG).show();
+        }
     }
 }
